07bb0470329650df7bbd67b3548e6a03487960f4
Luis Amesty Linares <luisamesty@gmail.com>
2020-08-17 10:05:50 +0200

    Header Comments added

--- idempiere-special-editor-examples/src/org/idempiere/extend/SpecialEditC_InvoiceC_Activity_ID.java
+++ idempiere-special-editor-examples/src/org/idempiere/extend/SpecialEditC_InvoiceC_Activity_ID.java
@@ -1,6 +1,6 @@
 /******************************************************************************
- * Copyright (C) 2012 Carlos Ruiz                                             *
- * Copyright (C) 2012 Trek Global                 							  *
+ * Copyright (C) 2015 Luis Amesty                                             *
+ * Copyright (C) 2015 AMERP Consulting                                        *
  * This program is free software; you can redistribute it and/or modify it    *
  * under the terms version 2 of the GNU General Public License as published   *
  * by the Free Software Foundation. This program is distributed in the hope   *
@@ -9,8 +9,7 @@
  * See the GNU General Public License for more details.                       *
  * You should have received a copy of the GNU General Public License along    *
  * with this program; if not, write to the Free Software Foundation, Inc.,    *
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
- *****************************************************************************/
+ ******************************************************************************/
 package org.idempiere.extend;
 
 import java.sql.Timestamp;
--- idempiere-special-editor-examples/src/org/idempiere/extend/SpecialEditC_InvoiceDateAcct.java
+++ idempiere-special-editor-examples/src/org/idempiere/extend/SpecialEditC_InvoiceDateAcct.java
@@ -1,3 +1,15 @@
+/******************************************************************************
+ * Copyright (C) 2015 Luis Amesty                                             *
+ * Copyright (C) 2015 AMERP Consulting                                        *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ ******************************************************************************/
 package org.idempiere.extend;
 
 import java.sql.Timestamp;
